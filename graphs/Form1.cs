599bac98b0914796dc785474ce51c2ef86b5a6ec
UswaArif <uswaarif32@gmail.com>
2022-11-24 11:58:08 +0500

    vb

---
+++ graphs/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void plotbtn_Click(object sender, EventArgs e)
+        {
+            chart.Series["Series"].Points.AddXY("Nor", 8.7);
+            chart.Series["Series"].Points.AddXY("Noir", 8.9);
+            chart.Series["Series"].Points.AddXY("or", 9.7);
+        }
+    }
+}
